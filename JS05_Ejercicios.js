5022f87b158d9781681d3b2605ae6b9138169ee8
AndreaRamirez17 <illeand2001@gmail.com>
2024-04-26 19:57:09 -0600

    avance2

--- JS05_Ejercicios.js
+++ JS05_Ejercicios.js
@@ -1,26 +1,43 @@
-/**
- * Perfil del usuario
- * Escriba un programa que solicite al usuario sus datos: nombre de usuario, edad y una lista de sus películas favoritas. Almacene la información y luego muéstrela en la consola. Tenga en cuenta que el resultado de las películas debe agregar un pequeño mensaje como: 'La película {película} es una de mis favoritas'.
- */
+//Perfil del usuario
+
 let userData= prompt("Ingresa tú nombre de usuario: ");
 let userAge=prompt("Ingresa tú edad: ");
 let userMovies=prompt("Ingresa tú lista de peliculas favoritas separadas por comas: ");
-const moviesArr=userMovies.split(',');
+const moviesArr=userMovies.split(' ');
 var rndmIndex = Math.floor(Math.random() * moviesArr.length); //moviesArray
 var rndmMovie = moviesArr[rndmIndex];
 console.log(`Mi nombre de usuario es ${userData}, tengo ${userAge} años de edad y la película ${rndmMovie} es una de mis favoritas`);
 
 //-------------------------------------------------------------------
-/**
- * Numero mas alto
- * Escribe un programa que solicite 10 números. Usando operadores lógicos y cualquier otra función/estructura de JavaScript que haya visto antes, determine y genere el mayor de esos números.
- */
+// Numero mas alto
+//Este ejemplo se imprime en alertas, no sé si sea una buena practica, pero me gustó el resultado, algo diferente a solo publicarlo en la consola :)
+
+let shouldRetry;
+do { 
+let serieNum=prompt("Ingrese una serie de 10 numeros:")
+const newArr=serieNum.split(' ');
+if (newArr.length == [10]){
+let array1=[];
+let numArray=newArr.forEach((e)=>{array1.push(Number(e))});
+const max=Math.max.apply(null, array1);
+alert("El numero maximo es:  "+ max);
+shouldRetry = confirm("¿Deseas volver a intentarlo con otra serie de numeros?");
+}else if (newArr.length < [10]){
+   alert("Ingresaste menos de 10 numeros, vuele a intentarlo");
+    shouldRetry = true; 
+}
+else{
+    alert("Ingresaste más de 10 numeros,vuelve a intentarlo");
+    shouldRetry = true; 
+}
+}while (shouldRetry);
 //-------------------------------------------------------------------
 /**
  * Alarma
  * Escribe un programa que pregunte a un usuario la cantidad de segundos necesarios hasta que se ejecute una alarma (mensaje) junto con un texto para mostrar una vez que hayan pasado esos segundos en tiempo real.
  * Ejemplo de resultado : "Hora de acostarse después de 10 segundos".
  */
+
 //-------------------------------------------------------------------
 /**
  * Palíndromo
@@ -29,8 +46,24 @@ console.log(`Mi nombre de usuario es ${userData}, tengo ${userAge} años de edad
 //-------------------------------------------------------------------
 /**
  * Factorial
- * Escriba un programa que solicite un número entero n . Donde  1 <= norte. Resuelve esto usando recursividad.
+ * Escriba un programa que solicite un número entero n . Donde  1 <= n. Resuelve esto usando recursividad.
  */
+let m=prompt("Para determinar el factorial, ingrese un numero: ");
+function factorial(n) {
+    if (n === 1) {
+        return 1;
+    }
+   
+    else if (n > 1) {
+        return n * factorial(n - 1);
+    }
+    else{
+        console.log("no calculo el factorial de 0 o de numeros negativos")
+    }
+}
+console.log(factorial(m));
+
+
 //-------------------------------------------------------------------
 /**
  * matriz plana
